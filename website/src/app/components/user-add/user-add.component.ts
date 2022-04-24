9a88951e6ac356c1ac5f270ef178d782b9059e4d
JackRStiles <tehskrunch@outlook.com>
2022-04-24 23:03:08 +0100

    Started working on adding a user

--- website/src/app/components/user-add/user-add.component.ts
+++ website/src/app/components/user-add/user-add.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../../services/users.service';
+import { Items } from '../../intefaces/users';
+import { FormControl } from '@angular/forms';
+import { catchError, last, of } from 'rxjs';
 
 @Component({
   selector: 'app-user-add',
   templateUrl: './user-add.component.html',
-  styleUrls: ['./user-add.component.scss']
+  styleUrls: ['./user-add.component.scss'],
 })
 export class UserAddComponent implements OnInit {
+  errorMessage: any;
+  lastID = 0;
 
-  constructor() { }
+  constructor(private usersService: UsersService) {}
 
-  ngOnInit(): void {
+  onSubmit(data: any) {
+    let now = new Date().toISOString().slice(0, 10);
+    let dob = new Date(data.dateOfBirth).toISOString().slice(0, 10);
+    let user = {
+      id: this.lastID + 1,
+      birthDate: dob,
+      firstName: data.firstName,
+      lastName: data.lastName,
+      gender: data.gender.charAt(0),
+      created: now,
+    };
+
+    console.log(user);
+
+    this.usersService.addUser(user);
   }
 
+  ngOnInit() {
+    this.usersService.getAllUsers().subscribe((data: Items) => {
+      let total = data.total;
+
+      this.usersService.getUsers(total, 0).subscribe((data: Items) => {
+        let items = data;
+        this.lastID = items.items[total-1].id; 
+      });
+    });
+  }
 }
